69fefa562cc8c2e16e1cdf1234ba1ea486b23d4e
momoladebrouill <momoladebrouill@gmail.com>
2024-01-10 14:57:31 +0100

    Cleaner 🧑‍⚖️🫶️

--- 3NN.py
+++ 3NN.py
@@ -11,16 +11,12 @@ class Pos():
 
     
 def nearestat(event):
-    pos=Pos(event.x,event.y)
-    dists_black=[]
-    for bpos in black:
-        dists_black.append(bpos.dist(pos))
-    dists_blue=[]
-    for bpos in blue:
-        dists_blue.append(bpos.dist(pos))
+    pos = Pos(event.x,event.y)
+    dists_black = [bpos.dist(pos) for bpos in black]
+    dists_blue = [bpos.dist(pos) for bpos in blue]
     dists_blue.sort()
     dists_black.sort()
-    orders=[]
+    orders = []
     for _ in range(3):
         if dists_blue[0]<dists_black[0]:
             dists_blue.pop(0)
@@ -28,17 +24,16 @@ def nearestat(event):
         else:
             dists_black.pop(0)
             orders.append("black")
-    result="black" if orders.count("black")>orders.count("blue") else "blue"
+    result = "black" if orders.count("black") > orders.count("blue") else "blue"
     can.create_rectangle(pos.x-5,pos.y-5,pos.x+5,pos.y+5,fill=result,tags=('jui',),outline="white")
     return result
 
 
 def grid(event):
-    step=10
+    step = 10
     for y in range(0,500+step,step):
         for x in range(0,500+step,step):
             nearestat(Pos(x,y))
-
             
 def circle(posa,ray,coul):
     return can.create_oval(posa.x*10+ray,posa.y*10+ray,
@@ -48,52 +43,44 @@ class self():
     _drag_data = {"x": 0, "y": 0, "item": None}
 
 def drag_start(event):
-    """Begining drag of an object"""
-    # record the item and its location
     self._drag_data["item"] = can.find_closest(event.x, event.y)[0]
     self._drag_data["x"] = event.x
     self._drag_data["y"] = event.y
 
 def drag_stop(event):
-    """End drag of an object"""
-    # reset the drag information
     for pos in blue+black:
         if pos.tag==self._drag_data["item"]:
             pos.x,pos.y=self._drag_data["x"],self._drag_data["y"]
-        
     self._drag_data["item"] = None
     self._drag_data["x"] = 0
     self._drag_data["y"] = 0
 
 def drag(event):
-    """Handle dragging of an object"""
-    # compute how much the mouse has moved
     delta_x = event.x - self._drag_data["x"]
     delta_y = event.y - self._drag_data["y"]
-    # move the object the appropriate amount
     can.move(self._drag_data["item"], delta_x, delta_y)
-    # record the new position
     self._drag_data["x"] = event.x
     self._drag_data["y"] = event.y
+
 def erase(event):
     can.delete("jui")
 
     
-black=[Pos(15,49),Pos(11,18),Pos(9,25)]
-blue= [Pos(17,22),Pos(28,20),Pos(19,17)]
+black = [Pos(15,49),Pos(11,18),Pos(9,25)]
+blue = [Pos(17,22),Pos(28,20),Pos(19,17)]
 
-f=tk.Tk()
+f = tk.Tk()
 f.title('The 3 Nearest Neighbord')
-can=tk.Canvas(f,width=500,height=500)
+can = tk.Canvas(f,width=500,height=500)
 can.pack()
 for posa,posb in zip(black,blue):
-    posa.tag=circle(posa,10,"black")
-    posb.tag=circle(posb,10,"blue")
+    posa.tag = circle(posa,10,"black")
+    posb.tag = circle(posb,10,"blue")
 can.bind('<Button-2>',lambda event:print(nearestat(event)))
 can.tag_bind("mouvable", "<ButtonPress-1>", drag_start)
 can.tag_bind("mouvable", "<ButtonRelease-1>", drag_stop)
 can.tag_bind("mouvable", "<B1-Motion>", drag)
 f.bind('a',grid)
 f.bind('e',erase)
-
+input()
     
